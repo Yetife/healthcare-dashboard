c6bf8883620757466422188f0885cf0867f50476
Yetife <tifetreats21@gmail.com>
2025-02-13 16:10:55 +0100

    appointment update

--- src/components/AppointmentDetails.jsx
+++ src/components/AppointmentDetails.jsx
@@ -5,13 +5,16 @@ const AppointmentDetails = ({ appointment }) => {
 
 
     return (
-        <div className="p-4 border rounded-lg shadow-md">
-            <h2 className="text-xl font-bold">{appointment.patient_name}</h2>
-            <p><strong>Doctor:</strong> {appointment.doctor_name}</p>
-            <p><strong>Date:</strong> {appointment.date_time.split('T')[0]}</p>
-            <p><strong>Time:</strong> {formatTime(appointment.date_time)}</p>
-            <p><strong>Clinic:</strong> {appointment.clinic_location}</p>
+        <div>
+            <div className="p-4 border rounded-lg shadow-md">
+                <h3 className="text-xl font-bold"><strong>Patient Name:</strong> {appointment.patient_name}</h3>
+                <p><strong>Doctor:</strong> {appointment.doctor_name}</p>
+                <p><strong>Date:</strong> {appointment.date_time.split('T')[0]}</p>
+                <p><strong>Time:</strong> {formatTime(appointment.date_time)}</p>
+                <p><strong>Clinic:</strong> {appointment.clinic_location}</p>
+            </div>
         </div>
+
     );
 };
 
--- src/pages/AppointmentPage.jsx
+++ src/pages/AppointmentPage.jsx
@@ -8,8 +8,9 @@ const AppointmentPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="p-6 bg-gradient-to-r from-indigo-500 to-purple-500 min-h-screen text-white">
+        <div className="p-6 bg-gradient-to-r from-indigo-300 to-purple-300 min-h-screen text-white">
             <button onClick={() => navigate(-1)} className="mb-4 bg-gray-800 text-white px-3 py-1 rounded hover:bg-gray-600">← Back</button>
+            <h1 className="md:text-3xl text-xl font-bold text-center mb-6">Patient Details</h1>
             <div className="bg-white p-6 rounded-lg shadow-lg max-w-2xl mx-auto text-gray-900">
                 {appointment ? (
                     <>
